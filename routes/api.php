9b6932b82e3c5df798634a05f67e74377f7fc295
anaradiocta <nelaocta12@gmail.com>
2024-11-05 21:40:55 +0700

    Praktikum 3 - Jobsheet 10

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
 
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
